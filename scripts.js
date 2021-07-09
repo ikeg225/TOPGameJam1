c4260e0832bcdc50258b54fb2f137b593853fae5
Dustin Brown <dustin.brown.k@gmail.com>
2021-07-09 19:19:52 -0400

    Basic Setup

---
+++ scripts.js
@@ -0,0 +1,25 @@
+/*The Odin Project -- Game Jam 2021
+    SquidSquad
+    coderlore
+    iketaco
+    AqueousOtter
+
+    Game Specs:
+        Random word is chosen and characters are randomly jumbled
+        Word is presented to player
+        Player mixes the letter
+        If the player’s word is equal to the original word, the win/get a point
+        If the player’s word is NOT equal to the original word, the lose/lose one chance or heart
+*/
+
+//set up local storage with default score list
+
+
+//arrays/objects with default words
+
+//array/object logic to handle user adding custom list
+
+//main game functions
+
+//main game logic
+
